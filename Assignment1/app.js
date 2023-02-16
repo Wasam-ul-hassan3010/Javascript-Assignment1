8de167166c39e8b0d8e14cff6726ed76f74cad09
Wasam-ul-hassan3010 <124611992+Wasam-ul-hassan3010@users.noreply.github.com>
2023-02-16 19:46:52 +0500

    Add files via upload

---
+++ Assignment1/app.js
@@ -0,0 +1,21 @@
+// CHAPTER 01 ALERTS //
+
+
+// *Question 1*
+// alert("Wasam");
+
+
+// *Question 02*
+// alert("Error! please enter a valid password");
+
+
+// *Question 03*
+// alert("Welcome to JS land... \n Happy coding!");
+
+// // *Question 04*
+// alert("Welcome to JS Land...");
+// alert("Happy coding! \n Prevent this page from creating additional dialogs")
+
+
+// *Qustion 05*
+// alert("Hello...I can run JS through my web browser's console");
